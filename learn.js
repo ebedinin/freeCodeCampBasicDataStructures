4f717b481cc1e6284f41bb41a78ae5ff4477f89b
Admin <bedinin@gmail.com>
2021-11-24 16:48:50 +1000

    15

--- learn.js
+++ learn.js
@@ -1,5 +1,13 @@
-let myArray = ["a", "b", "c", "d"];
-// Only change code below this line
-myArray[1] = "y";
-// Only change code above this line
-console.log(myArray);
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift("three");
+    arr.unshift(2);
+    arr.unshift("I");
+    arr.push(7);
+    arr.push("VIII");
+    arr.push(9);
+    // Only change code above this line
+    return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
