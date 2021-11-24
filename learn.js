a2e2083c621c864343c37965e527aaf4e50a1d83
Admin <bedinin@gmail.com>
2021-11-24 17:28:14 +1000

    30

--- learn.js
+++ learn.js
@@ -1,7 +1,7 @@
-function popShift(arr) {
-    let popped = arr.pop(); // Change this line
-    let shifted = arr.shift(); // Change this line
-    return [shifted, popped];
-}
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+// Only change code above this line
 
-console.log(popShift(['challenge', 'is', 'not', 'complete']));
+arr.splice(0,1);
+arr.splice(3,4);
+console.log(arr);
