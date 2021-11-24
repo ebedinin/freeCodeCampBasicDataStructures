34b71aaebc1845f5c96b4df760dacc00ccfbd220
ebedinin <bedinin@gmail.com>
2021-11-24 20:59:21 +1000

    40

--- learn.js
+++ learn.js
@@ -1,7 +1,12 @@
-function popShift(arr) {
-    let popped = arr.pop(); // Change this line
-    let shifted = arr.shift(); // Change this line
-    return [shifted, popped];
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        // Only change code below this line
+        newArr.push([...arr]);
+        // Only change code above this line
+        num--;
+    }
+    return newArr;
 }
 
-console.log(popShift(['challenge', 'is', 'not', 'complete']));
+console.log(copyMachine([true, false, true], 2));
