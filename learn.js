63569cf0bd6fc0f1ce77604837b8b305f4823995
Admin <bedinin@gmail.com>
2021-11-24 16:51:20 +1000

    20

--- learn.js
+++ learn.js
@@ -1,13 +1,7 @@
-function mixedNumbers(arr) {
-    // Only change code below this line
-    arr.unshift("three");
-    arr.unshift(2);
-    arr.unshift("I");
-    arr.push(7);
-    arr.push("VIII");
-    arr.push(9);
-    // Only change code above this line
-    return arr;
+function popShift(arr) {
+    let popped = arr.pop(); // Change this line
+    let shifted = arr.shift(); // Change this line
+    return [shifted, popped];
 }
 
-console.log(mixedNumbers(['IV', 5, 'six']));
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
