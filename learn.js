453081e3f3518f35caf916ddbfee0c55b6a9c723
Admin <bedinin@gmail.com>
2021-11-24 16:27:31 +1000

    10

--- learn.js
+++ learn.js
@@ -1,1 +1,5 @@
-let yourArray=[false,"false",0,1,2];
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "y";
+// Only change code above this line
+console.log(myArray);
