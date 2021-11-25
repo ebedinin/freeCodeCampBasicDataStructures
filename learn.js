232d9ffa8cc3a3f305f338d01a0bdd9d6e41154c
Admin <bedinin@gmail.com>
2021-11-25 14:59:52 +1000

    55

--- learn.js
+++ learn.js
@@ -1,7 +1,8 @@
-function quickCheck(arr, elem) {
-    // Only change code below this line
-
-    return arr.indexOf(elem) >= 0;
+function filteredArray(arr, elem) {
+    let newArr = [];
+    for(let i=0;i<arr.length;i++){
+        if (arr[i].indexOf(elem) === -1) newArr.push(arr[i]);
+    }
+    return newArr;
 }
-
-console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
