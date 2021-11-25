f3e0ffa89f640ee66fb3771fe111c1708b1001b1
Admin <bedinin@gmail.com>
2021-11-25 15:24:34 +1000

    60

--- learn.js
+++ learn.js
@@ -1,8 +1,9 @@
-function filteredArray(arr, elem) {
-    let newArr = [];
-    for(let i=0;i<arr.length;i++){
-        if (arr[i].indexOf(elem) === -1) newArr.push(arr[i]);
-    }
-    return newArr;
-}
-console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+let myNestedArray = [
+    // change code below this line
+    ["unshift", false, 1, 2, 3, "complex", "nested"],
+    ["loop", "shift", 6, 7, 1000, "method"],
+    ["concat", false, true, "spread", "array", ["deep"]],
+    ["mutate", 1327.98, "splice", "slice", "push", [["deeper"]]],
+    ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth", [[["deepest"]]]]
+    // change code above this line
+];
