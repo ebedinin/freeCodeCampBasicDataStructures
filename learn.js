25264eafb0cdd805eede0de31e5f3138ff77b179
ebedinin <bedinin@gmail.com>
2021-11-27 00:05:17 +1000

    95

--- learn.js
+++ learn.js
@@ -1,9 +1,30 @@
-function countOnline(usersObj) {
-    // Only change code below this line
-    let count=0;
-    for(let item in usersObj){
-        if(usersObj[item].online) count+=1;
+let users = {
+    Alan: {
+        age: 27,
+        online: false
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: false
+    },
+    Ryan: {
+        age: 19,
+        online: true
     }
-    return count;
+};
+
+function getArrayOfUsers(obj) {
+    // Only change code below this line
+    const arr = [];
+    for(let item in obj){
+        arr.push(item);
+    }
+    return arr;
     // Only change code above this line
 }
+
+console.log(getArrayOfUsers(users));
